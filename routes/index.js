ad483ac58eab6adef901becbe7f6f40e3ead898c
ben <ben.matt.dunn@live.ca>
2016-10-03 19:31:29 -0400

    figured out a partial hiarchy developed where scripting can be changed rapidily in sub elemets, switched over

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,10 @@
+/*
+
+
+*/
+
+
+
 var express = require('express');
 var router = express.Router();
 var showSubContainer = 'false';
@@ -13,22 +20,12 @@ router.get('/', function(req, res, next) {
         'Secondary languages include: Html, Php, SQL',
         firstDiv: true,
         secondDiv: false,
-        pictureLead: true,
-        contact: false,
+        pictureLead: true
     });
 });
 
 router.get('/contact', function(req, res, next) {
-    res.render('index', {   title: 'Ben Matt Dunn',
-        message: 'Ben Dunn is currently a student at Georgian enrolled in the COPR program,' +
-
-        'Currently working within the aerospace field as a scientific programmer, specifically within the optical branch and have considerable experience linking micro controllers to computers to control devices that range from spectrometers, spectrographs, and scientific light sources'+
-        'Previously attaining a BA of psychology from Laurentian University. Having achieved excellence in mathematics and calculus as side courses, learning a generalized knowledge base that includes and is not limited to English studies, basic chemistry, biology and research methods.'+
-        'Primary languages known include: Labview, C, C++, C#, Java' +
-        'Secondary languages include: Html, Php, SQL',
-        firstDiv: false,
-        secondDiv: false,
-        pictureLead: false,
+    res.render('contact', {   title: 'Ben Matt Dunn',
         contact: true
     });
 });
