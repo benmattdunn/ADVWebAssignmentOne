86d766d0ec033b8e61fd66527132ec5ff11b886a
ben <ben.matt.dunn@live.ca>
2016-10-03 18:23:27 -0400

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var showSubContainer = 'false';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lesson 4',
-   });
+    res.render('index', {   title: 'Ben Matt Dunn',
+                            message: 'Ben Dunn is currently a student at Georgian enrolled in the COPR program, \n' +
+
+        'Currently working within the aerospace field as a scientific programmer, specifically within the optical branch and have considerable experience linking micro controllers to computers to control devices that range from spectrometers, spectrographs, and scientific light sources'+
+        'Previously attaining a BA of psychology from Laurentian University. Having achieved excellence in mathematics and calculus as side courses, learning a generalized knowledge base that includes and is not limited to English studies, basic chemistry, biology and research methods.'+
+        'Primary languages known include: Labview, C, C++, C#, Java'+
+        'While secondary languages include: Html, Php, SQL'
+                            +'\n',
+
+        firstDiv: true,
+        secondDiv: false,
+        pictureLead: true
+    });
 });
 
-
-/* get random page */
-router.get('/random', function(req, res, next){
-  var randomNum = Math.random();
-
-  // render the random number
-  res.render('random', {title: 'random number getter',
-    randomNumber: randomNum});
-});
 
 module.exports = router;
