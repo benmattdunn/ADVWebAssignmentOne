4c7c7333f034af0fbc16ec7e5c2a70712fcc93bc
ben <ben.matt.dunn@live.ca>
2016-10-03 20:10:31 -0400

    added icons from bootstrap and cleaned up soe basic code, removed second div portion in simple div. Added js files from boots strap in public for added functionality, changed aboutMe link to index to remove a linking error

--- routes/index.js
+++ routes/index.js
@@ -11,22 +11,28 @@ var showSubContainer = 'false';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index'||'', {   title: 'Ben Matt Dunn',
-                            message: 'Ben Dunn is currently a student at Georgian enrolled in the COPR program,' +
+    res.render('index'||'', {   WStitle: 'Ben Matt Dunn',
+        messageSimpleDev: 'Ben Dunn is currently a student at Georgian enrolled in the COPR program,' +
 
         'Currently working within the aerospace field as a scientific programmer, specifically within the optical branch and have considerable experience linking micro controllers to computers to control devices that range from spectrometers, spectrographs, and scientific light sources'+
         'Previously attaining a BA of psychology from Laurentian University. Having achieved excellence in mathematics and calculus as side courses, learning a generalized knowledge base that includes and is not limited to English studies, basic chemistry, biology and research methods.'+
         'Primary languages known include: Labview, C, C++, C#, Java' +
         'Secondary languages include: Html, Php, SQL',
         firstDiv: true,
-        secondDiv: false,
         pictureLead: true
     });
 });
 
 router.get('/contact', function(req, res, next) {
-    res.render('contact', {   title: 'Ben Matt Dunn',
-        contact: true
+    res.render('contact', {   WStitle: 'Ben Matt Dunn',
+        contactVisable: true
+    });
+});
+
+router.get('/projectsStart', function(req, res, next) {
+    res.render('projectsStart', {   WStitle: 'Ben Matt Dunn',
+        carouselTitleVisable: false,
+        carouselTitle: ''
     });
 });
 
