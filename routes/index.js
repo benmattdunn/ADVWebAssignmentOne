12cb5482a11e71422c3bc20f6c5fc722cbdf3fbe
ben <ben.matt.dunn@live.ca>
2016-10-12 14:58:02 -0400

    final commit with all links created

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,32 @@ var express = require('express');
 var router = express.Router();
 var showSubContainer = 'false';
 
-/* GET home page. */
+/* Get Home Page */
 router.get('/', function(req, res, next) {
-    res.render('index'||'', {   WStitle: 'Ben Matt Dunn',
+    res.render('index', {   WStitle: 'Ben Matt Dunn',
+        JumboTitle: 'Welcome~!',
+        JumboText: 'Welcome to mysite, here I post work projects and such (not really Rich, but this is a sample example for' +
+        'coding purposes, I will definitivily create a website in the future, but once I know enough to actually create a great' +
+        'working one)',
+        JumboButtonLink: '/aboutMe',
+        JumboButtonLinkText: 'About Me'
+    });
+});
+/* --> just notes for quick reference.
+ JumboTitle -> string
+ JumboText -> string
+ JumboButtonLink -> string <must be a valid address ->
+ JumboButtonLinkText -> String (should link to the name of the valid address page)
+
+
+
+ */
+
+
+
+/* GET aboutMe page. */
+router.get('/aboutMe', function(req, res, next) {
+    res.render('aboutMe', {   WStitle: 'Ben Matt Dunn',
         messageSimpleDev: 'Ben Dunn is currently a student at Georgian enrolled in the COPR program,' +
 
         'Currently working within the aerospace field as a scientific programmer, specifically within the optical branch and have considerable experience linking micro controllers to computers to control devices that range from spectrometers, spectrographs, and scientific light sources'+
@@ -26,12 +49,14 @@ router.get('/', function(req, res, next) {
     });
 });
 
+/* Get contact page */
 router.get('/contact', function(req, res, next) {
     res.render('contact', {   WStitle: 'Ben Matt Dunn',
         contactVisable: true
     });
 });
 
+/*get projects page */
 router.get('/projectsStart', function(req, res, next) {
     res.render('projectsStart', {   WStitle: 'Ben Matt Dunn',
         carouselTitleVisable: false,
@@ -39,6 +64,7 @@ router.get('/projectsStart', function(req, res, next) {
     });
 });
 
+/* get services page */
 router.get('/services', function(req, res, next){
     res.render('services', {
         WStitle: 'Ben Matt Dunn'
